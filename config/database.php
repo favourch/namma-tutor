35e1a1e223d7e6f30def9265c1017f769106c431
Prince Ali Khan <prince@zoojoo.be>
2016-08-27 01:22:33 +0530

    Unit Test Fix

--- config/database.php
+++ config/database.php
@@ -85,6 +85,19 @@ return [
             'prefix'   => '',
         ],
 
+
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
