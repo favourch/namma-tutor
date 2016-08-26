26bcd628b767da145f7959ff77f3fc9e93cac96c
Prince Ali Khan <prince@zoojoo.be>
2016-08-26 12:32:19 +0530

    Environment Configuration

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'Z2eGyIgtgxl8dKhyb3Kdo7EunkJGKv1Y'),
 
     'cipher' => 'AES-256-CBC',
 
